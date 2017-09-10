2b226033701fd577fbcb3385c6f1b9c5d3af6755
Heitor Souza <hsouza@Avenues-MacBook-Pro.local>
2017-09-10 20:05:19 -0300

    ball init

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	Rigidbody2D ball;
+	public float force;
+
+	// Use this for initialization
+	void Start () {
+		ball = GetComponent<Rigidbody2D> ();
+		ball.AddForce (new Vector2 (1f, 0.5f) * Time.deltaTime * force);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
