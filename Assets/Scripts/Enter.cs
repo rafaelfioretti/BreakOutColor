7e1d2a19916e782b6e4b90a0fe60049c9f5ec638
Heitor Souza <hsouza@Avenues-MacBook-Pro.local>
2017-09-13 21:21:46 -0300

    add intro scene

---
+++ Assets/Scripts/Enter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Enter : MonoBehaviour {
+
+	public Button btnInit;
+	Button btn;
+
+	// Use this for initialization
+	void Start () {
+		btn = btnInit.GetComponent<Button> ();
+		btn.onClick.AddListener (EnterGame);
+	}
+	
+		
+	void EnterGame(){
+		SceneManager.LoadScene ("Game");
+	}
+}
